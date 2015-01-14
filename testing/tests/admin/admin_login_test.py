21cc3991bc5fd57d5aeec60fe442e531a2ad87ec
muggle95 <melmel195@yahoo.com>
2015-01-14 11:42:54 -1000

    added folders for organization; moved some existing files into them.

---
+++ testing/tests/admin/admin_login_test.py
@@ -0,0 +1,83 @@
+#! usr/bin/python
+
+# this is just the presence-of (buttons, text fields...) tests, not the functionality tests
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re
+
+class AdminLoginTest(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.PhantomJS("/usr/local/bin/phantomjs")
+        self.driver.implicitly_wait(30)
+        self.base_url = "http://localhost"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+    
+    def test_admin_login(self):
+        driver = self.driver
+        driver.get(self.base_url + "/~mschmock/Contest/admin/index.php")
+        user = driver.find_element_by_name("user")
+        user.clear()
+        user.send_keys("admin")
+        driver.find_element_by_name("password").clear()
+        driver.find_element_by_name("password").send_keys("password")
+        driver.find_element_by_name("submit").click()
+        try: self.assertEqual("Edit Contest Info", driver.find_element_by_css_selector("div.table-responsive > table.table > tbody > tr").text)
+        except AssertionError as e: self.verificationErrors.append(str(e))
+        driver.get(self.base_url + "/~mschmock/Contest/admin/index.php")
+        driver.find_element_by_name("user").clear()
+        driver.find_element_by_name("user").send_keys("admin")
+        driver.find_element_by_name("password").clear()
+        driver.find_element_by_name("password").send_keys("admin")
+        driver.find_element_by_name("submit").click()
+        try: self.assertRegexpMatches(driver.find_element_by_css_selector("div.row").text, r"^Admin Login[\s\S]*$")
+        except AssertionError as e: self.verificationErrors.append(str(e))
+        driver.get(self.base_url + "/~mschmock/Contest/admin/index.php")
+        driver.find_element_by_name("user").clear()
+        driver.find_element_by_name("user").send_keys("")
+        driver.find_element_by_name("password").clear()
+        driver.find_element_by_name("password").send_keys("password")
+        driver.find_element_by_name("submit").click()
+        try: self.assertRegexpMatches(driver.find_element_by_css_selector("div.row").text, r"^Admin Login[\s\S]*$")
+        except AssertionError as e: self.verificationErrors.append(str(e))
+        driver.get(self.base_url + "/~mschmock/Contest/admin/index.php")
+        driver.find_element_by_name("user").clear()
+        driver.find_element_by_name("user").send_keys("")
+        driver.find_element_by_name("password").clear()
+        driver.find_element_by_name("password").send_keys("")
+        driver.find_element_by_name("submit").click()
+        try: self.assertRegexpMatches(driver.find_element_by_css_selector("div.row").text, r"^Admin Login[\s\S]*$")
+        except AssertionError as e: self.verificationErrors.append(str(e))
+    
+    def is_element_present(self, how, what):
+        try: self.driver.find_element(by=how, value=what)
+        except NoSuchElementException, e: return False
+        return True
+    
+    def is_alert_present(self):
+        try: self.driver.switch_to_alert()
+        except NoAlertPresentException, e: return False
+        return True
+    
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally: self.accept_next_alert = True
+    
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+if __name__ == "__main__":
+    unittest.main()
